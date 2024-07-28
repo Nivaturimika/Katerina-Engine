7f5d448a9ac92ac6bc92da6c60ad23499fc9f892
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-07-28 05:48:13 +0100

    fix crime tooltip

--- src/gui/map_tooltip.cpp
+++ src/gui/map_tooltip.cpp
@@ -1214,8 +1214,8 @@ void crime_map_tt_box(sys::state& state, text::columnar_layout& contents, dcon::
 		text::substitution_map sub;
 		text::add_to_substitution_map(sub, text::variable_type::x, state.culture_definitions.crimes[state.world.province_get_crime(prov)].name);
 		text::localised_format_box(state, contents, box, std::string_view("mapmode_tooltip_41"), sub);
-		ui::modifier_description(state, contents, state.culture_definitions.crimes[state.world.province_get_crime(prov)].modifier);
 		text::close_layout_box(contents, box);
+		ui::modifier_description(state, contents, state.culture_definitions.crimes[state.world.province_get_crime(prov)].modifier);
 	}
 }
 void rally_map_tt_box(sys::state& state, text::columnar_layout& contents, dcon::province_id prov) {
