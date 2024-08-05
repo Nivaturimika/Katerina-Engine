e79c32482af9c55136468cff9f2ff6c5ba28b7eb
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-08-05 01:28:35 +0100

    FRANCE NOW HAS SURPLUS!?

--- src/economy/economy.cpp
+++ src/economy/economy.cpp
@@ -2092,7 +2092,7 @@ float full_pop_spending_cost(sys::state& state, dcon::nation_id n) {
 		}
 		assert(std::isfinite(total) && total >= 0.0f);
 	});
-	total *= (1.f / state.defines.alice_needs_scaling_factor);
+	total *= (0.5f / state.defines.alice_needs_scaling_factor);
 	assert(std::isfinite(total) && total >= 0.0f);
 	return total;
 }
@@ -3036,7 +3036,7 @@ void daily_update(sys::state& state, bool initiate_buildings) {
 		auto owner_spending = state.world.nation_get_spending_level(owners);
 
 		auto pop_of_type = state.world.pop_get_size(ids);
-		auto adj_pop_of_type = pop_of_type * (1.f / state.defines.alice_needs_scaling_factor);
+		auto adj_pop_of_type = pop_of_type * (0.5f / state.defines.alice_needs_scaling_factor);
 
 		auto const a_spending = owner_spending * ve::to_float(state.world.nation_get_administrative_spending(owners)) / 100.f;
 		auto const s_spending = owner_spending * state.world.nation_get_administrative_efficiency(owners) * ve::to_float(state.world.nation_get_social_spending(owners)) / 100.0f;
@@ -3087,7 +3087,7 @@ void daily_update(sys::state& state, bool initiate_buildings) {
 		auto employment = state.world.pop_get_employment(ids);
 
 		acc_u = acc_u + ve::select(none_of_above && state.world.pop_type_get_has_unemployment(types),
-			s_spending * (pop_of_type - employment) * (1.f / state.defines.alice_needs_scaling_factor) * unemp_level * ln_costs, 0.0f);
+			s_spending * (pop_of_type - employment) * (0.5f / state.defines.alice_needs_scaling_factor) * unemp_level * ln_costs, 0.0f);
 
 		state.world.pop_set_savings(ids, state.world.pop_get_savings(ids) + ((acc_e + acc_m) + (acc_u + acc_a)));
 		ve::apply([](float v) { assert(std::isfinite(v) && v >= 0.f); }, acc_e);
@@ -4081,7 +4081,7 @@ float estimate_pop_payouts_by_income_type(sys::state& state, dcon::nation_id n, 
 			total += adj_pop_of_type * state.world.nation_get_luxury_needs_costs(n, pt);
 		}
 	});
-	return total * (1.f / state.defines.alice_needs_scaling_factor);
+	return total * (0.5f / state.defines.alice_needs_scaling_factor);
 }
 
 float estimate_tax_income_by_strata(sys::state& state, dcon::nation_id n, culture::pop_strata ps) {
@@ -4165,7 +4165,7 @@ float estimate_domestic_investment(sys::state& state, dcon::nation_id n) {
 		* state.world.nation_get_luxury_needs_costs(n, state.culture_definitions.capitalists)
 		+ state.world.nation_get_demographics(n, demographics::to_key(state, state.culture_definitions.aristocrat))
 		* state.world.nation_get_luxury_needs_costs(n, state.culture_definitions.aristocrat))
-		* (1.f / state.defines.alice_needs_scaling_factor);
+		* (0.5f / state.defines.alice_needs_scaling_factor);
 }
 
 float estimate_land_spending(sys::state& state, dcon::nation_id n) {
