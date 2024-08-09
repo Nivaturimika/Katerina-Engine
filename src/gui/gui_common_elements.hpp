0a3f8789986ad1832c7b1e14b8ff0f2075f8decc
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-08-09 08:25:03 +0100

    fx?

--- src/gui/gui_common_elements.hpp
+++ src/gui/gui_common_elements.hpp
@@ -2172,16 +2172,16 @@ public:
 				state.ui_state.root->add_child_to_front(std::move(win));
 			}
 			if(state.ui_state.national_focus_overwrite_window) {
-				state.ui_state.national_focus_overwrite_window->set_visible(state, true);
 				state.ui_state.national_focus_overwrite_window->base_data.position = get_absolute_non_mirror_location(state, *this);
 				if(state.ui_state.national_focus_overwrite_window->base_data.position.y + state.ui_state.national_focus_overwrite_window->base_data.size.y > state.y_size) {
 					state.ui_state.national_focus_overwrite_window->base_data.position.y -= state.ui_state.national_focus_overwrite_window->base_data.size.y;
 				} else {
 					state.ui_state.national_focus_overwrite_window->base_data.position.y += base_data.size.y;
 				}
-				state.ui_state.root->move_child_to_front(state.ui_state.national_focus_overwrite_window);
 				static_cast<national_focus_overwrite_window*>(state.ui_state.national_focus_overwrite_window)->target = national_focus_overwrite_target{ content, nat_focus };
+				state.ui_state.national_focus_overwrite_window->set_visible(state, true);
 				state.ui_state.national_focus_overwrite_window->impl_on_update(state);
+				state.ui_state.root->move_child_to_front(state.ui_state.national_focus_overwrite_window);
 			}
 		} else {
 			command::set_national_focus(state, state.local_player_nation, content, nat_focus);
