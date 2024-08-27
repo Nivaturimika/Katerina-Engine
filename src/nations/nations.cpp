d3730312555ef8889b77cea2a5ae1e2673af7a98
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-08-27 07:37:48 +0100

    factor leaders into calc

--- src/nations/nations.cpp
+++ src/nations/nations.cpp
@@ -454,7 +454,11 @@ void update_military_scores(sys::state& state) {
 		auto disarm_factor = ve::select(is_disarmed, ve::fp_vector(disarm), ve::fp_vector(1.0f));
 		auto supply_mod = ve::max(state.world.nation_get_modifier_values(n, sys::national_mod_offsets::supply_consumption) + 1.0f, 0.1f);
 		auto avg_land_score = state.world.nation_get_averge_land_unit_score(n);
-		state.world.nation_set_military_score(n, ve::to_int((ve::min(recruitable, active_regs * 4.0f) * avg_land_score) * ((disarm_factor * supply_mod) / 7.0f) + state.world.nation_get_capital_ship_score(n) + active_regs));
+		auto num_leaders = ve::apply([&](dcon::nation_id i) {
+			auto ll = state.world.nation_get_leader_loyalty(i);
+			return int32_t(ll.end() - ll.begin());
+		})
+		state.world.nation_set_military_score(n, ve::to_int((ve::min(recruitable, active_regs * 4.0f) * avg_land_score) * ((disarm_factor * supply_mod) / 7.0f) + state.world.nation_get_capital_ship_score(n) + ve::max(num_leaders, active_regs)));
 	});
 }
 
