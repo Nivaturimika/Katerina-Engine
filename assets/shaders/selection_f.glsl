4636f8059e0cfead29b8db3d08d9a22f3586d8d5
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-09-21 06:02:35 +0100

    fix capitals in huper

--- assets/shaders/selection_f.glsl
+++ assets/shaders/selection_f.glsl
@@ -6,7 +6,7 @@ uniform sampler2D texture_sampler;
 in vec2 tc;
 
 void main() {
-	vec4 colour = texture(texture_sampler, tc);
+	vec4 colour = texture(texture_sampler, vec2(tc.x, 1.f - tc.y));
 	colour.a *= (colour.r + colour.g + colour.b) == 0.f ? 0.f : 1.f;
 	frag_color = gamma_correct(colour);
 }
