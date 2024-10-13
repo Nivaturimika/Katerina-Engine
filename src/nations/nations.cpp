2c24e67b8812b282ab278bdb16ecf916b15c3484
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-10-13 06:18:59 +0100

    fix

--- src/nations/nations.cpp
+++ src/nations/nations.cpp
@@ -3177,9 +3177,7 @@ namespace nations {
 					state.world.delete_gp_relationship(rel);
 				}
 			} else {
-				auto rels = n.get_gp_relationship_as_great_power();
-				while(rels.begin() != rels.end()) {
-					auto rel = *(rels.begin());
+				for(const auto rel : n.get_gp_relationship_as_great_power()) {
 					if((rel.get_status() & influence::level_mask) == influence::level_in_sphere) {
 						rel.get_influence_target().set_in_sphere_of(rel.get_great_power());
 					}
