51bbccfcca81920981777e40779af0e3c428fedf
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-10-18 21:37:06 +0100

    fixups

--- src/common_types/reports.hpp
+++ src/common_types/reports.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
 #include <string_view>
+#include <stdio.h>
+
 namespace reports {
 	inline void write_debug(std::string_view msg) noexcept {
 		if(msg.size() > 0) {
 			std::string s = std::string(msg);
 #ifdef _WIN32
-			static FILE* fp = fopen("game_log.txt", "wt");
+			static FILE* fp = NULL;
+			if(!fp) {
+				fopen_s(&fp, "game_log.txt", "wt");
+			}
 			fprintf(fp, "%s", s.c_str());
 			OutputDebugStringA(s.c_str());
 #else
