1806b78316ed77a2c2d06069311e464c328ef002
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-10-19 16:59:34 +0100

    remove body from reports

--- src/common_types/reports.hpp
+++ src/common_types/reports.hpp
@@ -3,23 +3,5 @@
 #include <string_view>
 
 namespace reports {
-	void write_debug(std::string_view msg) noexcept {
-		if(msg.size() > 0) {
-			std::string s = std::string(msg);
-#ifdef _WIN32
-			static FILE* fp = NULL;
-			static bool tried_opening_fp = false;
-			if(!fp && !tried_opening_fp) {
-				fopen_s(&fp, "game_log.txt", "wt");
-				tried_opening_fp = true;
-			}
-			if(fp) {
-				fprintf(fp, "%s", s.c_str());
-			}
-			OutputDebugStringA(s.c_str());
-#else
-			std::fprintf(stderr, "%s", s.c_str());
-#endif
-		}
-	}
+	void write_debug(std::string_view msg) noexcept;
 }
