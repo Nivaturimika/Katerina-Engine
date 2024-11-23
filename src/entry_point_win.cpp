1ff8c81fff80e2cdc1c33b926cc7a48187f118d3
Nivaturimika <danishreyjavik@outlook.com>
2024-11-23 06:52:33 +0000

    fix windows and mac?

--- src/entry_point_win.cpp
+++ src/entry_point_win.cpp
@@ -17,10 +17,6 @@
 #pragma comment(lib, "Ole32.lib")
 #pragma comment(lib, "Shell32.lib")
 
-#pragma comment(lib, "icudt.lib")
-#pragma comment(lib, "icuin.lib")
-#pragma comment(lib, "icuuc.lib")
-
 #include "entry_point.cpp"
 
 static CRITICAL_SECTION guard_abort_handler;
