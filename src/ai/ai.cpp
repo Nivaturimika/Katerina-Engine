a752806b2fbd588294edd5da07bef6c9ba4de05d
Nivaturimika <danishreyjavik@outlook.com>
2024-12-17 00:59:18 +0000

    smarter industrializing ai

--- src/ai/ai.cpp
+++ src/ai/ai.cpp
@@ -703,7 +703,10 @@ namespace ai {
 			static std::vector<dcon::state_instance_id, dcon::cache_aligned_allocator<dcon::state_instance_id>> ordered_states;
 			ordered_states.clear();
 			for(auto si : n.get_state_ownership()) {
-				ordered_states.push_back(si.get_state().id);
+				auto total = si.get_state().get_demographics(demographics::total);
+				if(total > 0.f) {
+					ordered_states.push_back(si.get_state().id);
+				}
 			}
 			pdqsort(ordered_states.begin(), ordered_states.end(), [&](auto a, auto b) {
 				auto apop = state.world.state_instance_get_demographics(a, demographics::total);
@@ -712,11 +715,12 @@ namespace ai {
 					return apop > bpop;
 				return a.index() < b.index();
 			});
-			bool threatened = n.get_ai_is_threatened() || n.get_is_at_war();
 			for(uint32_t i = 0; num_focuses_total > 0 && i < ordered_states.size(); ++i) {
 				auto prov = state.world.state_instance_get_capital(ordered_states[i]);
-				if(max_clergy) {
-					if(threatened) {
+				auto total = state.world.state_instance_get_demographics(ordered_states[i], demographics::total);
+				auto cfrac = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_key(state, state.culture_definitions.clergy)) / total;
+				if(cfrac >= base_opt) {
+					if(n.get_is_at_war()) {
 						auto nf = state.national_definitions.soldier_focus;
 						auto k = state.world.national_focus_get_limit(nf);
 						if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
@@ -725,87 +729,68 @@ namespace ai {
 							--num_focuses_total;
 						}
 					} else {
-						auto total = state.world.state_instance_get_demographics(ordered_states[i], demographics::total);
-						auto cfrac = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_key(state, state.culture_definitions.clergy)) / total;
-						if(cfrac < state.defines.max_clergy_for_literacy * 0.8f) {
-							auto nf = state.national_definitions.clergy_focus;
+						auto pw_num = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_key(state, state.culture_definitions.primary_factory_worker));
+						auto pw_employed = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_employment_key(state, state.culture_definitions.primary_factory_worker));
+						auto sw_num = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_key(state, state.culture_definitions.secondary_factory_worker));
+						auto sw_employed = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_employment_key(state, state.culture_definitions.secondary_factory_worker));
+						auto pw_frac = pw_num / (pw_num + sw_num);
+						auto sw_frac = sw_num / (pw_num + sw_num);
+						auto ideal_pwfrac = state.economy_definitions.craftsmen_fraction;
+						auto ideal_swfrac = (1.f - state.economy_definitions.craftsmen_fraction);
+						// Due to floating point comparison where 2.9999 != 3, we will round the number
+						// so that the ratio is NOT exact, but rather an aproximate
+						if(pw_employed >= pw_num && int8_t(pw_frac * 100.f) != int8_t(ideal_pwfrac * 100.f)) {
+							auto nf = state.national_definitions.secondary_factory_worker_focus;
 							auto k = state.world.national_focus_get_limit(nf);
 							if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
+								// Keep balance between ratio of factory workers
+								// we will only promote primary workers if none are unemployed
 								assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
-								state.world.state_instance_set_owner_focus(ordered_states[i], state.national_definitions.clergy_focus);
+								state.world.state_instance_set_owner_focus(ordered_states[i], nf);
 								--num_focuses_total;
+							}
+						} else if(sw_employed >= sw_num && int8_t(sw_frac * 100.f) != int8_t(ideal_swfrac * 100.f)) {
+							auto nf = state.national_definitions.primary_factory_worker_focus;
+							auto k = state.world.national_focus_get_limit(nf);
+							if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
+								// Keep balance between ratio of factory workers
+								// we will only promote secondary workers if none are unemployed
+								assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
+								state.world.state_instance_set_owner_focus(ordered_states[i], nf);
+								--num_focuses_total;
+							}
+						} else {
+							/* If we are a civilized nation, and we allow pops to operate on the economy
+							i.e Laissez faire, we WILL promote capitalists, since they will help to
+							build new factories for us */
+							auto rules = n.get_combined_issue_rules();
+							if(n.get_is_civilized() && (rules & (issue_rule::pop_build_factory | issue_rule::pop_build_factory_invest | issue_rule::pop_expand_factory | issue_rule::pop_expand_factory_invest | issue_rule::pop_open_factory | issue_rule::pop_open_factory_invest)) != 0) {
+								auto nf = state.national_definitions.capitalist_focus;
+								auto k = state.world.national_focus_get_limit(nf);
+								if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
+									assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
+									state.world.state_instance_set_owner_focus(ordered_states[i], nf);
+									--num_focuses_total;
+								}
+							} else {
+								auto nf = state.national_definitions.aristocrat_focus;
+								auto k = state.world.national_focus_get_limit(nf);
+								if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
+									assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
+									state.world.state_instance_set_owner_focus(ordered_states[i], nf);
+									--num_focuses_total;
+								}
 							}
 						}
 					}
-				} else {
+				} else { //cfrac < base_opt
 					// If we haven't maxxed out clergy on this state, then our number 1 priority is to maximize clergy
-					auto cfrac = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_key(state, state.culture_definitions.clergy)) / state.world.state_instance_get_demographics(ordered_states[i], demographics::total);
-					if(cfrac < base_opt * 1.2f) {
-						auto nf = state.national_definitions.clergy_focus;
-						auto k = state.world.national_focus_get_limit(nf);
-						if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
-							assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
-							state.world.state_instance_set_owner_focus(ordered_states[i], state.national_definitions.clergy_focus);
-							--num_focuses_total;
-						}
-					}
-				}
-			}
-
-			for(uint32_t i = 0; num_focuses_total > 0 && i < ordered_states.size(); ++i) {
-				auto prov = state.world.state_instance_get_capital(ordered_states[i]);
-				auto total = state.world.state_instance_get_demographics(ordered_states[i], demographics::total);
-				auto pw_num = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_key(state, state.culture_definitions.primary_factory_worker));
-				auto pw_employed = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_employment_key(state, state.culture_definitions.primary_factory_worker));
-				auto sw_num = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_key(state, state.culture_definitions.secondary_factory_worker));
-				auto sw_employed = state.world.state_instance_get_demographics(ordered_states[i], demographics::to_employment_key(state, state.culture_definitions.secondary_factory_worker));
-				auto pw_frac = pw_num / (pw_num + sw_num);
-				auto sw_frac = sw_num / (pw_num + sw_num);
-				auto ideal_pwfrac = state.economy_definitions.craftsmen_fraction;
-				auto ideal_swfrac = (1.f - state.economy_definitions.craftsmen_fraction);
-				// Due to floating point comparison where 2.9999 != 3, we will round the number
-				// so that the ratio is NOT exact, but rather an aproximate
-				if(pw_employed >= pw_num && int8_t(pw_frac * 100.f) != int8_t(ideal_pwfrac * 100.f)) {
-					auto nf = state.national_definitions.secondary_factory_worker_focus;
+					auto nf = state.national_definitions.clergy_focus;
 					auto k = state.world.national_focus_get_limit(nf);
 					if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
-						// Keep balance between ratio of factory workers
-						// we will only promote primary workers if none are unemployed
 						assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
-						state.world.state_instance_set_owner_focus(ordered_states[i], nf);
+						state.world.state_instance_set_owner_focus(ordered_states[i], state.national_definitions.clergy_focus);
 						--num_focuses_total;
-					}
-				} else if(sw_employed >= sw_num && int8_t(sw_frac * 100.f) != int8_t(ideal_swfrac * 100.f)) {
-					auto nf = state.national_definitions.primary_factory_worker_focus;
-					auto k = state.world.national_focus_get_limit(nf);
-					if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
-						// Keep balance between ratio of factory workers
-						// we will only promote secondary workers if none are unemployed
-						assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
-						state.world.state_instance_set_owner_focus(ordered_states[i], nf);
-						--num_focuses_total;
-					}
-				} else {
-					/* If we are a civilized nation, and we allow pops to operate on the economy
-					   i.e Laissez faire, we WILL promote capitalists, since they will help to
-					   build new factories for us */
-					auto rules = n.get_combined_issue_rules();
-					if(n.get_is_civilized() && (rules & (issue_rule::pop_build_factory | issue_rule::pop_build_factory_invest | issue_rule::pop_expand_factory | issue_rule::pop_expand_factory_invest | issue_rule::pop_open_factory | issue_rule::pop_open_factory_invest)) != 0) {
-						auto nf = state.national_definitions.capitalist_focus;
-						auto k = state.world.national_focus_get_limit(nf);
-						if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
-							assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
-							state.world.state_instance_set_owner_focus(ordered_states[i], nf);
-							--num_focuses_total;
-						}
-					} else {
-						auto nf = state.national_definitions.aristocrat_focus;
-						auto k = state.world.national_focus_get_limit(nf);
-						if(!k || trigger::evaluate(state, k, trigger::to_generic(prov), trigger::to_generic(n), -1)) {
-							assert(command::can_set_national_focus(state, n, ordered_states[i], nf));
-							state.world.state_instance_set_owner_focus(ordered_states[i], nf);
-							--num_focuses_total;
-						}
 					}
 				}
 			}
@@ -910,9 +895,6 @@ namespace ai {
 			return false;
 		if(politics::is_election_ongoing(state, n))
 			return false;
-		// Do not appoint if we are a democracy!
-		if(politics::has_elections(state, n))
-			return false;
 		return true;
 	}
 
@@ -941,7 +923,9 @@ namespace ai {
 				float max_support = 0.f; //estimate_pop_party_support(state, n, state.world.nation_get_ruling_party(n));
 				for(int32_t i = start; i < end; i++) {
 					auto pid = dcon::political_party_id(uint16_t(i));
-					if(pid != state.world.nation_get_ruling_party(n) && politics::political_party_is_active(state, n, pid) && (gov.get_ideologies_allowed() & ::culture::to_bits(state.world.political_party_get_ideology(pid))) != 0) {
+					if(pid != state.world.nation_get_ruling_party(n)
+					&& politics::political_party_is_active(state, n, pid)
+					&& (gov.get_ideologies_allowed() & ::culture::to_bits(state.world.political_party_get_ideology(pid))) != 0) {
 						auto support = estimate_pop_party_support(state, n, pid);
 						if(support > max_support
 						&& ai::political_party_is_state_economy(state, pid)) {
@@ -1005,6 +989,24 @@ namespace ai {
 		return false;
 	}
 
+	void get_ordered_economy_states(sys::state& state, dcon::nation_id n, std::vector<dcon::state_instance_id>& ordered_states) {
+		// prepare a list of states
+		ordered_states.clear();
+		for(auto si : state.world.nation_get_state_ownership(n)) {
+			if(si.get_state().get_capital().get_is_colonial() == false) {
+				ordered_states.push_back(si.get_state().id);
+			}
+		}
+		pdqsort(ordered_states.begin(), ordered_states.end(), [&](auto a, auto b) {
+			auto apop = state.world.state_instance_get_demographics(a, demographics::total);
+			auto bpop = state.world.state_instance_get_demographics(b, demographics::total);
+			if(apop != bpop)
+				return apop > bpop;
+			return a.index() < b.index();
+		});
+		return;
+	}
+
 	void update_ai_econ_construction(sys::state& state) {
 		for(auto n : state.world.in_nation) {
 			// skip over: non ais, dead nations, and nations that aren't making money
@@ -1012,39 +1014,16 @@ namespace ai {
 				continue;
 			if(n.get_is_player_controlled())
 				continue; // to handle the logic of player building automation later
-
-			/*
-			if(n.get_spending_level() < 1.0f || n.get_last_treasury() >= n.get_stockpiles(economy::money))
-				continue;
-			*/
-
-			float base_income = economy_estimations::estimate_daily_income(state, n) + n.get_stockpiles(economy::money) / 365.f;
-			if(economy_estimations::estimate_construction_spending(state, n) > 0.5f * base_income)
-				continue;
-
-			//if our army is too small, ignore buildings:
 			if(uint16_t(0.25f * n.get_recruitable_regiments()) > n.get_active_regiments())
-				continue;
+				continue; //if our army is too small, ignore buildings:
 
 			auto treasury = n.get_stockpiles(economy::money);
-			int32_t max_projects = std::max(8, int32_t(treasury / 64000.0f));
+			int32_t max_projects = std::max(16, int32_t(n.get_owned_state_count()));
 			auto rules = n.get_combined_issue_rules();
 			if((rules & issue_rule::expand_factory) != 0 || (rules & issue_rule::build_factory) != 0) {
 				// prepare a list of states
 				static std::vector<dcon::state_instance_id> ordered_states;
-				ordered_states.clear();
-				for(auto si : n.get_state_ownership()) {
-					if(si.get_state().get_capital().get_is_colonial() == false) {
-						ordered_states.push_back(si.get_state().id);
-					}
-				}
-				pdqsort(ordered_states.begin(), ordered_states.end(), [&](auto a, auto b) {
-					auto apop = state.world.state_instance_get_demographics(a, demographics::total);
-					auto bpop = state.world.state_instance_get_demographics(b, demographics::total);
-					if(apop != bpop)
-						return apop > bpop;
-					return a.index() < b.index();
-				});
+				ai::get_ordered_economy_states(state, n, ordered_states);
 
 				// subsidize key industries
 				if((rules & issue_rule::can_subsidise) != 0) {
@@ -1068,7 +1047,6 @@ namespace ai {
 					for(auto si : ordered_states) {
 						if(max_projects <= 0)
 							break;
-
 						province::for_each_province_in_state_instance(state, si, [&](dcon::province_id p) {
 							for(auto f : state.world.province_get_factory_location(p)) {
 								if(!f.get_factory().get_unprofitable()
